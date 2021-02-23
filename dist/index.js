f028bdb52b0b779fe5889002f036b4b0c82a1785
Dem0nZ <cbpskov@gmail.com>
2021-02-24 02:37:36 +0300

    create CRUD for posts

--- dist/index.js
+++ dist/index.js
@@ -8,14 +8,58 @@ var _Post = require('./models/Post');
 
 var _Post2 = _interopRequireDefault(_Post);
 
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 _mongoose2.default.connect('mongodb://localhost/server-side');
 
-var post = new _Post2.default({
-    title: "Hello, first",
-    text: "Oh my. Work hard"
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
+app.use(_bodyParser2.default.json());
+
+app.post('/posts', function (req, res) {
+    var data = req.body;
+    var post = new _Post2.default({
+        title: data.title,
+        text: data.text
+    });
+    post.save().then(function () {
+        res.send({ status: "OK" });
+    });
 });
-post.save().then(function () {
-    return console.log('OK');
+app.get('/posts', function (req, res) {
+    _Post2.default.find().then(function (err, posts) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(posts);
+    });
+});
+app.delete('/posts/:id', function (req, res) {
+    _Post2.default.remove({
+        _id: req.params.id
+    }).then(function (post) {
+        try {
+            res.json({ status: 'DELETED' });
+        } catch (e) {
+            res.json(e);
+        }
+        // if (post) {
+        //
+        // } else {
+        //
+        // }
+    });
+});
+
+app.listen(3000, function () {
+    return console.log("Server work on port 3000");
 });
--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import mongoose from 'mongoose';
 import Post from './models/Post';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/server-side')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.post('/posts', ( req, res) => {
+    const data = req.body;
+    const  post = new Post({
+        title: data.title,
+        text: data.text
+    })
+    post.save().then( () => {
+        res.send({ status: "OK"})
+    })
+})
+app.get('/posts', (req, res) => {
+    Post.find().then(( err, posts) => {
+        if (err) {
+            res.send(err)
+        }
+        res.json(posts)
+    })
+})
+app.delete('/posts/:id', (req, res) => {
+    Post.remove({
+        _id: req.params.id
+    }).then( post => {
+        try {
+            res.json({ status: 'DELETED' })
+        } catch (e) {
+            res.json(e)
+        }
+    })
+})
+app.put('/posts/:id', (req, res) => {
+    Post.findByIdAndUpdate( req.params.id, {$set: req.body}, (err) => {
+        if (err) {
+            res.send(err)
+        }
+        res.json({ status: "UPDATE"})
+    })
+})
 
 
-mongoose.connect('mongodb://localhost/server-side');
-
-const  post = new Post({
-    title: "Hello, first",
-    text: "Oh my. Work hard"
-});
-post.save().then( () => console.log('OK'));
+app.listen(3000, () => console.log("Server work on port 3000"))
